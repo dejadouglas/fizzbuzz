ec8a80fa3efc2817d23fb9e613b4e9173660cf58
Deja Douglas <ddouglas@chicagotechacademy.org>
2013-11-25 13:20:47 -0600

    deja2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,5 @@
 class Calculator
-def Calculator(num)
-if num % 15 == 0
-'fizzbuzz'
-elseif num % 5 ==0
-'buzz'
-elseif num % 3 == 0
-'fizz'
-else
-num
-end
-end 
+	def calculate(num)
+	1
+	end
 end
